806796a3c9ee2d39d605d814f1e305762e082c52
David Gutierrez <d.gutierrez-moreno@tum.de>
2024-04-09 15:16:46 +0000

    actions removed

--- diffuser/sampling/policies.py
+++ diffuser/sampling/policies.py
@@ -7,7 +7,8 @@ import diffuser.utils as utils
 from diffuser.datasets.preprocessing import get_policy_preprocess_fn
 
 
-Trajectories = namedtuple('Trajectories', 'actions observations values')
+# Trajectories = namedtuple("Trajectories", "actions observations values")
+Trajectories = namedtuple("Trajectories", "observations values")
 
 
 class GuidedPolicy:
@@ -25,21 +26,24 @@ class GuidedPolicy:
         conditions = self._format_conditions(conditions, batch_size)
 
         ## run reverse diffusion process
-        samples = self.diffusion_model(conditions, guide=self.guide, verbose=verbose, **self.sample_kwargs)
+        samples = self.diffusion_model(
+            conditions, guide=self.guide, verbose=verbose, **self.sample_kwargs
+        )
         trajectories = utils.to_np(samples.trajectories)
-
         ## extract action [ batch_size x horizon x transition_dim ]
-        actions = trajectories[:, :, :self.action_dim]
-        actions = self.normalizer.unnormalize(actions, 'actions')
+        # actions = trajectories[:, :, : self.action_dim]
+        # actions = self.normalizer.unnormalize(actions, 'actions')
 
         ## extract first action
-        action = actions[0, 0]
+        # action = actions[0, 0]
 
-        normed_observations = trajectories[:, :, self.action_dim:]
-        observations = self.normalizer.unnormalize(normed_observations, 'observations')
+        normed_observations = trajectories[:, :, self.action_dim :]
+        observations = self.normalizer.unnormalize(normed_observations, "observations")
 
-        trajectories = Trajectories(actions, observations, samples.values)
-        return action, trajectories
+        # trajectories = Trajectories(actions, observations, samples.values)
+        trajectories = Trajectories(observations, samples.values)
+        # return action, trajectories
+        return trajectories
 
     @property
     def device(self):
@@ -50,12 +54,13 @@ class GuidedPolicy:
         conditions = utils.apply_dict(
             self.normalizer.normalize,
             conditions,
-            'observations',
+            "observations",
         )
-        conditions = utils.to_torch(conditions, dtype=torch.float32, device='cuda:0')
+        conditions = utils.to_torch(conditions, dtype=torch.float32, device="cuda:0")
         conditions = utils.apply_dict(
             einops.repeat,
             conditions,
-            'd -> repeat d', repeat=batch_size,
+            "d -> repeat d",
+            repeat=batch_size,
         )
         return conditions
